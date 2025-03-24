66e52c147346c2989a7d6bc3c6aa9ac4bd9d9f0e
Hightouch <cetinkaya.ozden@gmail.com>
2025-03-24 02:23:13 +0100

    remove packages

--- src/globalplugin.ts
+++ src/globalplugin.ts
@@ -5,44 +5,6 @@ import moment from 'moment';
 import numeral from 'numeral';
 import type { App } from 'vue';
 import { useAppStore } from './stores/app';
-import { Remarkable } from 'remarkable';
-import { SanitizeConfig } from './utils/sanitizeConfig';
-
-import DOMPurify from 'dompurify';
-import { imageRegex, instagramRegex, twitterRegex, usernameURLRegex, youtubeRegex } from './utils/regexHelpers';
-import { marked } from 'marked';
-
-const renderer = new marked.Renderer();
-
-renderer.hr = () => {
-  return '<hr class="custom-hr"/>'; // Add your custom class or styles here
-};
-marked.setOptions({
-  renderer: renderer,
-  breaks: true, // Enable GFM line breaks
-  gfm: true,    // Enable GFM (GitHub Flavored Markdown)
-  pedantic: false // Parse according to strict markdown rules
-});
-
-// Apply the plugin
-interface Props {
-  text: string,
-  className?: string,
-  highQualityPost?: boolean,
-  noImage?: boolean,
-  allowDangerousHTML?: boolean,
-  hideImages?: boolean,
-  breaks?: boolean,
-  isProxifyImages?: boolean
-  isNsfw?: boolean;
-
-};
-
-const config = SanitizeConfig({
-  large: true,
-  highQualityPost: true,
-  noImage: false,
-});
 
 
 export function setupGlobalMethods(app: App) {
@@ -73,44 +35,6 @@ export function setupGlobalMethods(app: App) {
     //       return parseFloat(SP.toString()).toFixed(precision);
     //     }
     //   },
-    app.config.globalProperties.$markdown = async (value: string): Promise<any> => {
-      let processedContent = value
-
-      processedContent = processedContent.replace(youtubeRegex, (url, videoId) => {
-        return `<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
-      });
-
-      processedContent = processedContent.replace(twitterRegex, (url, tweetId) => {
-        return `<blockquote class="twitter-tweet"><a href="https://twitter.com/user/status/${tweetId}"></a></blockquote><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`;
-      });
-
-      processedContent = processedContent.replace(instagramRegex, (url, postId) => {
-        return `<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/${postId}"></blockquote><script async defer src="//www.instagram.com/embed.js"></script>`;
-      });
-
-      processedContent = processedContent.replace(imageRegex, (url, imageUrl) => {
-        return `<img class="image-container" src="${url}"/>`;
-      });
-
-      let parsedHtml = await marked(processedContent);
-
-      let sanitizedHtml = DOMPurify.sanitize(parsedHtml, {
-        ALLOWED_TAGS: [
-          'div', 'iframe', 'del', 'a', 'p', 'b', 'i', 'q', 'br', 'ul', 'li', 'ol', 'img', 'strong',
-          'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'blockquote', 'pre', 'code', 'em', 'strong',
-          'center', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'strike', 'sup', 'sub'
-        ],
-        ALLOWED_ATTR: [
-          'src', 'href', 'height', 'width', 'allow', 'frameborder', 'allowfullscreen',
-          'class', 'data-username', 'data-instgrm-permalink'
-        ]
-      });
-      parsedHtml = parsedHtml.replace(usernameURLRegex, (username) => {
-        const userName = username;
-        return `<a href='${userName}'>${userName}</a>`;
-      });
-      return sanitizedHtml;
-    },
     app.config.globalProperties.$amount = (value: number | undefined): any => {
       return numeral(value).format('0.[000]a')
     },
--- src/utils/regexHelpers.ts
+++
@@ -1,9 +0,0 @@
-export const imageRegex = /https?:\/\/(?:[-a-zA-Z0-9._]*[-a-zA-Z0-9])(?::\d{2,5})?(?:[/?#](?:[^\s"'<>\][()]*[^\s"'<>\][().,])?(?:(?:\.(?:tiff?|jpe?g|gif|png|svg|ico)|ipfs\/[a-z\d]{40,})))/gi;
-
-export const usernameURLRegex = /@([^/^\s]+)/g;
-
-export const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|user\/(?:[A-Za-z0-9_]+\/)?)([A-Za-z0-9_-]{11})/gi;
-
-export const instagramRegex = /(?:https?:\/\/)?(?:www\.)?instagram\.com\/p\/([A-Za-z0-9_-]+)/gi;
-
-export const twitterRegex = /(?:https?:\/\/)?(?:www\.)?twitter\.com\/(?:[A-Za-z0-9_]+\/status\/|status\/)(\d+)/gi;
